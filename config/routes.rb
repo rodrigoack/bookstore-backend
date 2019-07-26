29d601d1ca854de26da7c4ae62fe80b8dec6f4c6
Rodrigo Cavichioli <rodrigoack@gmail.com>
2019-07-25 22:35:55 -0300

    Implement API versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :authors do
-    resources :books
-  end
+  namespace :api do
+    namespace :v1 do
+      resources :authors do
+        resources :books
+      end
 
-  root to: 'authors#index'
+      root to: 'authors#index'
+    end
+  end
 end
